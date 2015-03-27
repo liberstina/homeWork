d1e668d88669807826e7f58ed130c9ab0beed32e
Liberstina <liberstina@gmail.com>
2015-03-27 18:32:17 +0200

    Task 8 I/O underdone

---
+++ homeTask/src/lib/hw8/RunClass.java
@@ -0,0 +1,38 @@
+package lib.hw8;
+
+import com.sourceit.hometask.io.FileAlreadyPresentsException;
+import com.sourceit.hometask.io.FileCopyFailedException;
+
+import java.io.File;
+
+/**
+ * Created by Liberstina on 27.03.2015.
+ */
+public class RunClass {
+    public static void main(String[] args) throws FileCopyFailedException {
+      /*  ImplCopyFileFactory copyFF = new ImplCopyFileFactory();
+
+
+        com.sourceit.hometask.io.CopyFileStrategy simpleCopy = copyFF.createSimpleCopyFileStrategy();
+        com.sourceit.hometask.io.CopyFileStrategy bufferCopy = copyFF.createBufferedCopyFileStrategy();
+        com.sourceit.hometask.io.CopyFileStrategy channelsCopy = copyFF.createChannelsCopyFileStrategy();
+        com.sourceit.hometask.io.CopyFileStrategy filesCopy = copyFF.createFilesCopyFileStrategy();
+
+        String pathTxt = "C:\\Users\\Liberstina\\Private\\Compile.txt";
+        String pathDoc = "C:\\Users\\Liberstina\\Private\\Comp.doc";
+        String pathJpeg = "C:\\Users\\Liberstina\\Private\\IM0253.jpeg";
+
+        File txt = new File ("C:\\Users\\Liberstina\\Private\\Compile.txt");
+        File doc = new File ("C:\\Users\\Liberstina\\Private\\Comp.doc");
+        File jpeg = new File ("C:\\Users\\Liberstina\\Private\\IM0253.jpeg");
+
+
+        try{
+
+        }
+    }
+
+*/
+
+    }
+}
