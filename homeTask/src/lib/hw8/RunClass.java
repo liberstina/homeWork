1544cd1fe5fc0ffef94a45b842e90258c28de8aa
Liberstina <liberstina@gmail.com>
2015-03-29 02:08:10 +0200

    Task 8 I/O update according to your remarks

--- homeTask/src/lib/hw8/RunClass.java
+++ homeTask/src/lib/hw8/RunClass.java
@@ -9,30 +9,64 @@ import java.io.File;
  * Created by Liberstina on 27.03.2015.
  */
 public class RunClass {
-    public static void main(String[] args) throws FileCopyFailedException {
-      /*  ImplCopyFileFactory copyFF = new ImplCopyFileFactory();
+  public static void main(String[] args) throws FileCopyFailedException, FileAlreadyPresentsException {
+    ImplCopyFileFactory copyFF = new ImplCopyFileFactory();
 
 
-        com.sourceit.hometask.io.CopyFileStrategy simpleCopy = copyFF.createSimpleCopyFileStrategy();
-        com.sourceit.hometask.io.CopyFileStrategy bufferCopy = copyFF.createBufferedCopyFileStrategy();
-        com.sourceit.hometask.io.CopyFileStrategy channelsCopy = copyFF.createChannelsCopyFileStrategy();
-        com.sourceit.hometask.io.CopyFileStrategy filesCopy = copyFF.createFilesCopyFileStrategy();
+    com.sourceit.hometask.io.CopyFileStrategy simpleCopy = copyFF.createSimpleCopyFileStrategy();
+    com.sourceit.hometask.io.CopyFileStrategy bufferCopy = copyFF.createBufferedCopyFileStrategy();
+    com.sourceit.hometask.io.CopyFileStrategy channelsCopy = copyFF.createChannelsCopyFileStrategy();
+    com.sourceit.hometask.io.CopyFileStrategy filesCopy = copyFF.createFilesCopyFileStrategy();
+/*
+input data path files
+C:\Users\Liberstina\Private\Compile.txt
+ */
+    String pathMain = "D:\\java.txt"; //for convinient using method for copying
+    String pathSimple = "D:\\javaSimple.txt";
+    String pathBuff = "D:\\javaBuff.txt";
+    String pathChann = "D:\\javaChann.txt";
+    String pathFiles = "D:\\javaFiles.txt";
 
-        String pathTxt = "C:\\Users\\Liberstina\\Private\\Compile.txt";
-        String pathDoc = "C:\\Users\\Liberstina\\Private\\Comp.doc";
-        String pathJpeg = "C:\\Users\\Liberstina\\Private\\IM0253.jpeg";
+      /*
+destination data files
+ */
+    File txtMain = new File("D:\\java.txt");
+    File txtSimple = new File("D:\\javaSimple.txt");
+    File txtBuff = new File("D:\\javaBuff.txt");
+    File txtChann = new File("D:\\javaChann.txt");
+    File txtFiles = new File("D:\\javaFiles.txt");
 
-        File txt = new File ("C:\\Users\\Liberstina\\Private\\Compile.txt");
-        File doc = new File ("C:\\Users\\Liberstina\\Private\\Comp.doc");
-        File jpeg = new File ("C:\\Users\\Liberstina\\Private\\IM0253.jpeg");
+    try {
 
+      //Copying methods are realized
+      //SimpleCopy with timesheet
+      long startTimeSimple = System.nanoTime();
+      copyFF.createSimpleCopyFileStrategy().copyFile(pathMain, pathSimple);
+      long elapsedTimeSimple = System.nanoTime() - startTimeSimple;
+      System.out.println("Elapsed Time for Simple copying is " + (elapsedTimeSimple / 1000000000000.0) + " sec");
 
-        try{
+      //BuffCopy with timesheet
+     long startTimeBuff = System.nanoTime();
+      copyFF.createBufferedCopyFileStrategy().copyFile(pathMain, pathBuff);
+      long elapsedTimeBuff = System.nanoTime() - startTimeBuff;
+      System.out.println("Elapsed Time for Buffered copying is " + (elapsedTimeBuff / 1000000000000.0) + " sec");
 
-        }
+      //Channel with timesheet
+       long startTimeChann = System.nanoTime();
+      copyFF.createChannelsCopyFileStrategy().copyFile(pathMain, pathChann);
+      long elapsedTimeChann = System.nanoTime() - startTimeChann;
+      System.out.println("Elapsed Time for CopyFile copying is " + (elapsedTimeChann / 1000000000000.0) + " sec");
+
+      //CopyFile with timesheet
+      long startTimeCopyFile = System.nanoTime();
+      copyFF.createFilesCopyFileStrategy().copyFile(pathMain, pathFiles);
+      long elapsedTimeCopyFile = System.nanoTime() - startTimeCopyFile;
+      System.out.println("Elapsed Time for CopyFile copying is " + (elapsedTimeCopyFile / 1000000000000.0) + " sec");
+
+    } catch (FileAlreadyPresentsException e) {
+
+      System.out.println(e.getMessage());
+
     }
-
-*/
-
-    }
+  }
 }
